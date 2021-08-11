34f29667ceb847b5ef579cd50a1d3d063b8dcaf4
Heikki Jylhä <hjjylh@gmail.com>
2021-08-11 20:23:47 +0300

    added price calculator for console

--- calc.py
+++ calc.py
@@ -44,6 +44,14 @@ def read_age_values():
                 age_values[i]['modifier_classic'] = int(dataline[2])
     return age_values
 
+def add_empty_space_at_the_end(word, length):
+    if len(word) < length:
+        new_word = word
+        for _ in range(length - len(word)):
+            new_word = new_word + " "
+        return new_word
+    return word
+
 def rounding(num):
     return int((num // 10) // 10 * 10)
 
@@ -91,7 +99,26 @@ def calculate_max_price(ride_type, excitement, intensity, nausea, free_entry=Tru
     return max_prices
 
 def calculator():
-    pass
+    ride_name = input("What is the ride type? ")
+    print("Type the rating values of the ride without dots, i.e. multiplied by 100.")
+    excitement = int(input("Excitement rating of the ride: "))
+    intensity = int(input("Intensity rating of the ride: "))
+    nausea = int(input("Nausea rating of the ride: "))
+    ans = input("Do you also charge for park entry? ")
+    if ans.lower() == "yes" or ans.lower() == "y":
+        free_entry = False
+    else:
+        free_entry = True
+    max_prices = calculate_max_price(ride_name, excitement, intensity, nausea, free_entry)
+    print("\nThese are the maximum prices you can charge, depending on the age of the ride.\n")
+    print("Age\t", "Max price (openRCT2)", "Max price (classic)", sep="\t")
+    age_values = read_age_values()
+    for i, price_line in enumerate(max_prices):
+        from_ = add_empty_space_at_the_end(str(age_values[i]['from']), 3)
+        to_ = add_empty_space_at_the_end(str(age_values[i]['to']), 3)
+        print(from_ + " ... " + to_ + "\t", *price_line, sep="\t")
+
+
 
 
 if __name__ == "__main__":
