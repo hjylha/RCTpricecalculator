59bfb8e909983b34caf3aef74939e708897135f2
Heikki Jylhä <hjjylh@gmail.com>
2021-08-12 21:18:30 +0300

    started building gui with kivy

---
+++ main.py
@@ -0,0 +1,41 @@
+from kivy.app import App
+from kivy.metrics import dp
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+
+
+class MainScreen(BoxLayout):
+    pass
+
+class DescriptionText(BoxLayout):
+    pass
+
+class InputSection(GridLayout):
+    pass
+
+class RideSelection(BoxLayout):
+    pass
+
+class ExcitementSelection(BoxLayout):
+    pass
+
+class IntensitySelection(BoxLayout):
+    pass
+
+class NauseaSelection(BoxLayout):
+    pass
+
+class PayForEntrySelection(BoxLayout):
+    pass
+
+class PriceTable(GridLayout):
+    pass
+
+class MainWidget(Widget):
+    pass
+
+class RCTPriceCalculatorApp(App):
+    pass
+
+RCTPriceCalculatorApp().run()
