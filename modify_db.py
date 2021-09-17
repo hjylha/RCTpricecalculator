2a47325c527bb86f4815a9b14912a8b6ad2e89b1
Heikki Jylhä <hjjylh@gmail.com>
2021-09-17 11:58:10 +0300

    added fcn to set average EIN values as default

--- modify_db.py
+++ modify_db.py
@@ -1,6 +1,8 @@
+from os import stat
 from db_setup import create_default_EIN_columns, create_EIN_columns, ride_table_name, create_rides_columns
-from db_stuff import create_table, insert_data, select_rows_by_column_value, update_data_by_rowid, insert_data_and_create_table_if_not_created
+from db_stuff import create_table, insert_data, select_all, select_rows_by_column_value, update_data_by_rowid, insert_data_and_create_table_if_not_created
 from db_fcns import get_ride_names, find_ride_info, get_ride_rowid
+import statistics
 
 
 # saving EIN values to db
@@ -35,6 +37,25 @@ def update_default_values(ride_name, new_default_EIN):
     rowid = get_ride_rowid(ride_name)
     EIN_columns = create_default_EIN_columns()
     update_data_by_rowid(ride_table_name, EIN_columns, new_default_EIN, rowid)
+
+# calculate average EIN ratings
+def calculate_average_EIN(ride_name):
+    table = get_table_name_for_ride(ride_name)
+    all_data = select_all(table)
+    e_values, i_values, n_values = [], [], []
+    for data in all_data:
+        e_values.append(data[1])
+        i_values.append(data[2])
+        n_values.append(data[3])
+    e_average = statistics.mean(e_values)
+    i_average = statistics.mean(i_values)
+    n_average = statistics.mean(n_values)
+    return (e_average, i_average, n_average)
+
+# calculate average EIN and set them as default
+def set_average_values_as_default(ride_name):
+    average_EIN = calculate_average_EIN(ride_name)
+    update_default_values(ride_name, average_EIN)
 
 # add new row to ride table, which is a duplicate of an old row except for the name
 def add_alias(new_ride_name, old_ride_name):
