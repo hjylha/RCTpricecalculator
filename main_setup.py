1684b0a93e1a3a32ed35f66aba7416f3756ca41f
Heikki Jylhä <hjjylh@gmail.com>
2021-11-26 13:03:47 +0200

    changed suggestions back to list, and started tests on main_setup

--- main_setup.py
+++ main_setup.py
@@ -5,36 +5,33 @@ no_match_text = 'No match found'
 
 # try to find ride names containing given text_input
 def get_suggestions_for_ride_name(text_input: str, ride_names: dict, num_of_suggestions: int) -> list:
-    suggestions = set()
+    suggestions = []
     num = len(text_input)
     # match with start of ride name
     for name, vis_name in ride_names.items():
         if len(suggestions) >= num_of_suggestions:
-            # return list(suggestions)
-            break
-        if name.lower().startswith(text_input.lower()):
-            # suggestions.append(name)
-            suggestions.add(vis_name)
+            return suggestions
+            # break
+        if name.lower().startswith(text_input.lower()) and vis_name not in suggestions:
+            suggestions.append(vis_name)
     # match with any part of ride name
     for name, vis_name in ride_names.items():
         if len(suggestions) >= num_of_suggestions:
-            # return suggestions
-            break
+            return suggestions
+            # break
         if len(name) > num:
             for i in range(1, len(name)- num + 1):
                 if name[i:].lower().startswith(text_input.lower()):
-                    if name not in suggestions:
-                        # suggestions.append(name)
-                        suggestions.add(vis_name)
+                    if vis_name not in suggestions:
+                        suggestions.append(vis_name)
     # if nothing found, that is the suggestion
-    if suggestions == set():
-        # suggestions.append(no_match_text)
-        suggestions.add(no_match_text)
-    return list(suggestions)
+    if not suggestions:
+        suggestions.append(no_match_text)
+    return suggestions
 
 
 # if len(word) < num_of_letters, add two spaces for each 'missing' letter
-def add_double_spaces(word, num_of_letters):
+def add_double_spaces(word: str, num_of_letters: int) -> str:
     # if len(word) >= num_of_letters:
     #     return word
     formatted_word = word
@@ -43,22 +40,22 @@ def add_double_spaces(word, num_of_letters):
     return formatted_word
 
 # how to show age1 - age2 as a string
-def format_age_ranges(age1, age2):
+def format_age_ranges(age1: int, age2: int) -> str:
     text1 = add_double_spaces(str(age1), 3)
     text2 = add_double_spaces(str(age2), 3)
-    return text1 + '  ...  ' + text2
+    return f'{text1}  ...  {text2}'
 
 
 # divide by 100 and add spaces
-def price_as_string(price):
+def price_as_string(price: int) -> str:
     price_s = str(price)
     if price == 0:
         return '  0.00'
     if price < 100:
-        return '  0.' + price_s
+        return f'  0.{price_s}'
     if price < 1000:
-        return '  ' + price_s[0] + '.' + price_s[1:]
-    return price_s[:2] + '.' + price_s[2:]
+        return f'  {price_s[0]}.{price_s[1:]}'
+    return f'{price_s[:2]}.{price_s[2:]}'
 
 
 # the better the price, the greener the price
--- tests/test_main_setup.py
+++ tests/test_main_setup.py
@@ -2,6 +2,93 @@
 import fix_imports
 import main_setup
 
+
 def test_get_suggestions_for_ride_name():
+    names = {'name': 'visible_name',
+             'good name': 'visible_name',
+             'something else': 'nothing',
+             'alternative name': 'alt_name',
+             'zzz': 'zzz'}
+    num_of_suggestions = 3
+    text = ''
+    sugg = main_setup.get_suggestions_for_ride_name(text, names, num_of_suggestions)
+    assert sugg == ['visible_name', 'nothing', 'alt_name']
+
+    text = 'a'
+    sugg = main_setup.get_suggestions_for_ride_name(text, names, num_of_suggestions)
+    assert sugg == ['alt_name', 'visible_name']
+
+    text = 'q'
+    sugg = main_setup.get_suggestions_for_ride_name(text, names, num_of_suggestions)
+    assert sugg == [main_setup.no_match_text]
+
+    text = 'z'
+    sugg = main_setup.get_suggestions_for_ride_name(text, names, num_of_suggestions)
+    assert sugg == ['zzz']
+
+    text = 'name'
+    sugg = main_setup.get_suggestions_for_ride_name(text, names, num_of_suggestions)
+    assert sugg == ['visible_name', 'alt_name']
+
+    text = 'me'
+    sugg = main_setup.get_suggestions_for_ride_name(text, names, num_of_suggestions)
+    assert sugg == ['visible_name', 'nothing', 'alt_name']
+
+
+def test_add_double_spaces():
+    num_of_letters = 5
+    text = 'text12'
+    assert text == main_setup.add_double_spaces(text, num_of_letters)
+
+    text = 'text1'
+    assert text == main_setup.add_double_spaces(text, num_of_letters)
+
+    text = 'text'
+    assert 'text  ' == main_setup.add_double_spaces(text, num_of_letters)
+
+    text = 'tex'
+    assert 'tex    ' == main_setup.add_double_spaces(text, num_of_letters)
+
+
+def test_format_age_ranges():
+    age1 = 3
+    age2 = 10
+    assert '3      ...  10  ' == main_setup.format_age_ranges(age1, age2)
+
+    age1 = 13
+    age2 = 40
+    assert '13    ...  40  ' == main_setup.format_age_ranges(age1, age2)
+
+    age1 = 80
+    age2 = 110
+    assert '80    ...  110' == main_setup.format_age_ranges(age1, age2)
+
+    age1 = 200
+    age2 = ''
+    assert '200  ...        ' == main_setup.format_age_ranges(age1, age2)
+
+
+def test_price_as_string():
+    price = 0
+    assert '  0.00' == main_setup.price_as_string(price)
+
+    price = 40
+    assert '  0.40' == main_setup.price_as_string(price)
+
+    price = 420
+    assert '  4.20' == main_setup.price_as_string(price)
+
+    price = 1337
+    assert '13.37' == main_setup.price_as_string(price)
+
+
+def test_price_color():
     pass
 
+
+def test_get_EIN_value():
+    pass
+
+
+def test_get_EIN_values():
+    pass
